5a672cab53c7855fb863b447f85c147ae2d133d5
fand <fand.gmork@gmail.com>
2019-11-19 12:43:54 +0900

    init

---
+++ Assets/Mapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mapper : MonoBehaviour
+{
+    [SerializeField] Camera _sceneCamera;
+    [SerializeField] Transform _mapperTopLeft;
+    [SerializeField] Transform _mapperTopRight;
+    [SerializeField] Transform _mapperBottomLeft;
+    [SerializeField] Transform _mapperBottomRight;
+    [SerializeField] Material _material;
+
+    void Update()
+    {
+
+        Vector3 screenPos = _sceneCamera.WorldToScreenPoint(_mapperTopLeft.position);
+        _material.SetVector(0, screenPos);
+    }
+}
